88dffdeaf34188d7ef1bf5176db5edf41e55c497
maggydog <maggydog@mail.ru>
2022-06-16 17:47:17 +0300

    some fixes in code, add check for null

--- Test_task.cs
+++ Test_task.cs
@@ -7,15 +7,21 @@
 Console.WriteLine("Какой длины будет первоначальный массив?");
 int n;
 while (!int.TryParse(Console.ReadLine(), out n)) Console.WriteLine("Ошибка ввода. Введите целое положительное число");
+
 string[] initialArray = new string[n];
 
 FillArray(initialArray);
+Console.WriteLine();
 Console.WriteLine("Первоначальный массив:");
 Console.WriteLine(OtherWayToPrintArray(initialArray));
-
-Console.WriteLine("Новый массив:");
+Console.WriteLine();
 string[] finalArray = GetSomeFromArray(initialArray);
-PrintArray(finalArray);
+if (finalArray.Length == 0) Console.WriteLine("В певроначальном массиве нет элементов,длина которых меньше либо равна 3");
+else
+{
+    Console.WriteLine("Новый массив:");
+    PrintArray(finalArray);
+}
 
 
 //Задаем первоначальный массив строк через ввод вручную
@@ -46,7 +52,8 @@ string OtherWayToPrintArray(string[] array)
     result = "[ ";
     for (int i = 0; i < array.Length; i++)
     {
-        result += $"{array[i],3}";
+        if (i == array.Length - 1) result += $"{array[i],5}";
+        else result += $"{array[i]}, ";
     }
     result += " ]";
     return result;
@@ -56,12 +63,12 @@ string OtherWayToPrintArray(string[] array)
 // Тут сначала вытаскиваем длинну будущего массива, 
 // потом инициализируем его и заполняем элементами <=3.
 
-int HowLongNewArray (string [] oldArray)
+int HowLongNewArray(string[] oldArray)
 {
     int count = 0;
     for (int i = 0; i < oldArray.Length; i++)
     {
-        if (oldArray[i].Length <= 3) count++;
+        if (oldArray[i].Length <= 3 && !string.IsNullOrEmpty(oldArray[i])) count++;
     }
     return count;
 }
@@ -73,7 +80,7 @@ string[] GetSomeFromArray(string[] oldArray)
 
     for (int i = 0; i < oldArray.Length; i++)
     {
-        if (oldArray[i].Length <= 3)
+        if (oldArray[i].Length <= 3 && !string.IsNullOrEmpty(oldArray[i]))
         {
             newArray[j] = oldArray[i];
             j++;
