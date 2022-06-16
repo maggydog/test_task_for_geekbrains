a42731ad0bcafaeeea81b589cec15bba4c1952c8
maggydog <maggydog@mail.ru>
2022-06-16 15:21:22 +0300

    start work with cs file, create a brief plan for methods

--- Test_task.cs
+++ Test_task.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк 
+// сформирует массив из строк, длина которых меньше либо равна 3.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+
+//Задаем длину первоначального массива
+Console.WriteLine("Какой длины будет первоначальный массив?");
+int n;
+while (!int.TryParse(Console.ReadLine(), out n)) Console.WriteLine("Ошибка ввода. Введите целое положительное число");
+string[] initialArray = new string[n];
+
+FillArray(initialArray);
+Console.WriteLine("Первоначальный массив:");
+PrintArray(initialArray);
+
+Console.WriteLine("Новый массив:");
+string[] finalArray = GetSomeFromArray(initialArray);
+PrintArray(finalArray);
