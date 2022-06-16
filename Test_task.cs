83aa631a312235a7d690fd130c519ec605781591
maggydog <maggydog@mail.ru>
2022-06-16 15:41:11 +0300

    create get some from array method

--- Test_task.cs
+++ Test_task.cs
@@ -11,7 +11,7 @@ string[] initialArray = new string[n];
 
 FillArray(initialArray);
 Console.WriteLine("Первоначальный массив:");
-PrintArray(initialArray);
+Console.WriteLine(TruePrintArray(initialArray));
 
 Console.WriteLine("Новый массив:");
 string[] finalArray = GetSomeFromArray(initialArray);
@@ -36,4 +36,43 @@ void PrintArray(string[] array)
     {
         Console.WriteLine($"№{i}: {array[i]}");
     }
+}
+
+// Еще один метод печати
+
+string TruePrintArray(string[] array)
+{
+    string result = string.Empty;
+    result = "[ ";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += $"{array[i],3}";
+    }
+    result += " ]";
+    return result;
+}
+
+// Вытащим из этого массива другой, с длиной строк <=3. 
+// Тут сначала вытаскиваем длинну будущего массива, 
+// потом инициализируем его и заполняем элементами <=3.
+
+string[] GetSomeFromArray(string[] oldArray)
+{
+    int count = 0;
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= 3) count++;
+    }
+    string[] newArray = new string[count];
+    int j = 0;
+
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= 3)
+        {
+            newArray[j] = oldArray[i];
+            j++;
+        }
+    }
+    return newArray;
 }
